294d1f94f608a93dceedb58849dc617bdcb641d2
Shubham Pandit <Shubhampandit75572@gmail.com>
2025-03-23 12:33:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-// complete this js code
-class Person{
-	constructor(name, age) {
-		this._name = name;
-		this._age = age;
-	}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-	greet(){
-		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`)
-	}
+  greet() {
+    console.log(`Hello, my name is ${this._name} and I am ${this._age} years old.`);
+  }
 }
 
 class Employee extends Person {
-	constructor(name, age, jobTitle){
-		super(name,age)
-		this._jobTitle = jobTitle;
-	}
+  constructor(name, age, jobTitle) {
+    super(name, age);
+    this._jobTitle = jobTitle;
+  }
 
-	jobGreet(){
-		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`)
-	}
+  jobGreet() {
+    console.log(`Hello, my name is ${this._name} and I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
+  }
 }
 
 // Creating an instance of Person
 const person = new Person("Alice", 25);
-person.greet();
-// Expected Output: Hello, my name is Alice, I am 25 years old.
+person.greet(); 
+// Expected Output: Hello, my name is Alice and I am 25 years old.
 
-// Creating an instance of Employee
 const employee = new Employee("Bob", 30, "Manager");
-employee.jobGreet();
-// Expected Output: Hello, my name is Bob, I am 30 years old, and my job title is Manager.
+employee.jobGreet(); 
+// Expected Output: Hello, my name is Bob and I am 30 years old, and my job title is Manager.
 
 
 
